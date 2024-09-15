0ee2b49ca3d844479a85f82e001fb006d7a0239b
Lesley Caicedo G. <89746709+LesleyCaicedo@users.noreply.github.com>
2024-09-14 22:19:38 -0500

    Adicion componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponentComponent } from './components/auth-component/auth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponentComponent
   ],
   imports: [
     BrowserModule,
